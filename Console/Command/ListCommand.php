2225aeac01c23917d68d346d124fbba5f500e4bf
Steve Robbins <steve@steverobbins.com>
2016-04-09 14:36:15 -0700

    List all plugins in table format

--- Console/Command/ListCommand.php
+++ Console/Command/ListCommand.php
@@ -2,12 +2,23 @@
 
 namespace MagentoHackathon\PluginVisualization\Console\Command;
 
+use Magento\Framework\Module\ModuleListInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ListCommand extends Command
 {
+    private $_moduleList;
+
+    public function __construct(
+        ModuleListInterface $moduleList
+    ) {
+        $this->_moduleList = $moduleList;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->setName('dev:plugin:list')
@@ -16,6 +27,16 @@ class ListCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Hello world");
+        $plugins = $this->_moduleList->getNames();
+        sort($plugins);
+        $rows = [];
+        foreach ($plugins as $plugin) {
+            $rows[] = [$plugin];
+        }
+        $tableHelper = new Table($output);
+        $tableHelper
+            ->setHeaders(['Name'])
+            ->setRows($rows)
+            ->render();
     }
 }
