e233f04017b09826086dddd987ba70dadf69af4e
Kamil Balwierz <kamil@snow.dog>
2016-04-09 16:31:20 -0700

    WIP - attempt to get reflections on proxy obejcts

--- Console/Command/ListCommand.php
+++ Console/Command/ListCommand.php
@@ -2,12 +2,15 @@
 
 namespace MagentoHackathon\PluginVisualization\Console\Command;
 
+use Magento\Framework\Interception\PluginListInterface;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Setup\Module\Di\Code\Scanner\ConfigurationScanner;
 use MagentoHackathon\PluginVisualization\Model\Scanner\Plugin;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Zend\Server\Reflection\ReflectionClass;
 
 class ListCommand extends Command
 {
@@ -19,14 +22,26 @@ class ListCommand extends Command
      * @var Plugin
      */
     private $pluginScanner;
+    /**
+     * @var PluginListInterface
+     */
+    private $pluginList;
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
 
     public function __construct(
         ConfigurationScanner $configurationScanner,
-        Plugin $pluginScanner
+        Plugin $pluginScanner,
+        PluginListInterface $pluginList,
+        ObjectManagerInterface $objectManager
     ) {
         $this->configurationScanner = $configurationScanner;
-        parent::__construct();
         $this->pluginScanner = $pluginScanner;
+        $this->pluginList = $pluginList;
+        $this->objectManager = $objectManager;
+        parent::__construct();
     }
 
     protected function configure()
@@ -40,8 +55,22 @@ class ListCommand extends Command
         $files = $this->configurationScanner->scan('di.xml');
         $types = $this->pluginScanner->getAllTypes($files);
         $rows = [];
-        foreach ($types as $plugin) {
-            $rows[] = [$plugin];
+        foreach ($types as $type) {
+            try {
+                $proxy = $this->objectManager->get($type . '\\Proxy');
+                echo get_class($proxy) . "\n";
+                $reflection = new \ReflectionClass($proxy);
+                $methods = $reflection->getMethods();
+                foreach ($methods as $method) {
+                    var_dump($this->pluginList->getPlugin($type, $method->name));
+                }
+                $rows[] = [$type];
+            } catch (\Exception $e) {
+                echo $type;
+                echo "\t";
+                echo $e->getMessage();
+                echo "\n";
+            }
         }
         $tableHelper = new Table($output);
         $tableHelper
