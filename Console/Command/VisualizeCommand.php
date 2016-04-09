e9452b9690e94d87c42c9d035c0d0a68d9c2b86a
Anna Voelkl <anna@voelkl.at>
2016-04-09 16:20:38 -0700

    Update dev:plugin:visualize

--- Console/Command/VisualizeCommand.php
+++ Console/Command/VisualizeCommand.php
@@ -2,21 +2,30 @@
 
 namespace MagentoHackathon\PluginVisualization\Console\Command;
 
-use Magento\Framework\Module\ModuleListInterface;
+use Magento\Setup\Module\Di\Code\Scanner\ConfigurationScanner;
+use MagentoHackathon\PluginVisualization\Model\Scanner\Plugin;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class VisualizeCommand extends Command
 {
-    private $_moduleList;
+    /**
+     * @var ConfigurationScanner
+     */
+    private $configurationScanner;
+    /**
+     * @var Plugin
+     */
+    private $pluginScanner;
 
     public function __construct(
-        ModuleListInterface $moduleList
+        ConfigurationScanner $configurationScanner,
+        Plugin $pluginScanner
     ) {
-        $this->_moduleList = $moduleList;
+        $this->configurationScanner = $configurationScanner;
         parent::__construct();
+        $this->pluginScanner = $pluginScanner;
     }
 
     protected function configure()
@@ -27,5 +36,8 @@ class VisualizeCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $files = $this->configurationScanner->scan('di.xml');
+        $types = $this->pluginScanner->getAllClasses($files);
+        //@todo: continue here
     }
 }
