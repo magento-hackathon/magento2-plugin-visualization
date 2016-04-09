c43faef037fd1cc67de071fc2e554cda7f17e14a
Steve Robbins <steve@steverobbins.com>
2016-04-09 14:01:05 -0700

    Registering module

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MagentoHackaton_PluginVisualization',
+    __DIR__
+);
