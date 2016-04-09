db2c47aeaf98d111d85e9162bdbb1b9511d58cd2
Steve Robbins <steve@steverobbins.com>
2016-04-09 14:28:59 -0700

    "Hello world" in list command

--- Console/Command/ListCommand.php
+++ Console/Command/ListCommand.php
@@ -3,12 +3,19 @@
 namespace MagentoHackathon\PluginVisualization\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ListCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('dev:plugin:list');
-        $this->setDescription('List plugins');
+        $this->setName('dev:plugin:list')
+            ->setDescription('List plugins');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("Hello world");
     }
 }
