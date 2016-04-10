a70d631c831d7b511599d899327ea9b0674ccff4
Kamil Balwierz <kamil@snow.dog>
2016-04-09 18:30:09 -0700

    Tree drawing

--- Console/Command/ListCommand.php
+++ Console/Command/ListCommand.php
@@ -6,6 +6,7 @@ use Magento\Framework\Interception\PluginListInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Setup\Module\Di\Code\Scanner\ConfigurationScanner;
 use MagentoHackathon\PluginVisualization\Model\Scanner\Plugin;
+use PBergman\Console\Helper\TreeHelper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
@@ -29,36 +30,46 @@ class ListCommand extends Command
      * @var ObjectManagerInterface
      */
     private $objectManager;
+    /**
+     * @var TreeHelper
+     */
+    private $tree;
 
     public function __construct(
         ConfigurationScanner $configurationScanner,
         Plugin $pluginScanner,
         PluginListInterface $pluginList,
-        ObjectManagerInterface $objectManager
+        ObjectManagerInterface $objectManager,
+        TreeHelper $tree
     ) {
         $this->configurationScanner = $configurationScanner;
         $this->pluginScanner = $pluginScanner;
         $this->pluginList = $pluginList;
         $this->objectManager = $objectManager;
+        $this->tree = $tree;
         parent::__construct();
     }
 
-    protected function configure()
+    protected
+    function configure()
     {
         $this->setName('dev:plugin:list')
             ->setDescription('List plugins');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected
+    function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
         $files = $this->configurationScanner->scan('di.xml');
         $types = $this->pluginScanner->getAllTypes($files);
-        $rows = [];
         foreach ($types as $type) {
             try {
                 $proxy = $this->objectManager->get($type . '\\Proxy');
                 $reflection = new \ReflectionClass($proxy);
                 $methods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
+                $methodsFound = [];
                 foreach ($methods as $method) {
                     $methodName = $method->name;
                     $plugin = $this->pluginList->getNext($type, $methodName);
@@ -73,21 +84,23 @@ class ListCommand extends Command
                                 $plugins[] = $plug;
                             }
                         }
-                        $rows[] = [
-                            $type,
-                            $methodName,
-                            implode(', ', $plugins)
-                        ];
+                        $methodsFound[$methodName] = $plugins;
+                    }
+                }
+                if (!empty($methodsFound)) {
+                    $typeNode = $this->tree->newNode($type);
+                    foreach($methodsFound as $methodName => $plugins) {
+                        $methodNode = $typeNode->newNode($methodName);
+                        foreach($plugins as $plugin) {
+                            $methodNode->newNode($plugin);
+                            // TODO: add more data based on plugin name
+                        }
                     }
                 }
             } catch (\Exception $e) {
                 $output->writeln("Cannot analyze $type");
             }
         }
-        $tableHelper = new Table($output);
-        $tableHelper
-            ->setHeaders(['Type', 'Method', 'Plugins'])
-            ->setRows($rows)
-            ->render();
+        $this->tree->printTree($output);
     }
 }
